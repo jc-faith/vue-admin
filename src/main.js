cd3f2675927144d5cdcd302897c77b0fa2c63ef0
jc-faith <1527295607@qq.com>
2019-12-25 09:56:03 +0800

    实现国际化

--- src/main.js
+++ src/main.js
@@ -1,15 +1,21 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import elementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import Mock from '@/mock/index'
+import api from '@/http/index'
+import i18n from './i18n'
 
+Vue.use(elementUI)
+Vue.use(api)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  i18n,
   components: { App },
   template: '<App/>'
 })
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,67 @@
 import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import VueRouter  from 'vue-router'
+import login from '@/views/login'
+import home from '@/views/home'
+import NotFound from '@/views/error/404'
+import main from '@/views/main'
+import user from '@/views/user'
+import menu from '@/views/menu'
+import dept from '@/views/dept'
+import role from '@/views/role'
+import log from '@/views/log'
 
-Vue.use(Router)
+Vue.use(VueRouter)
 
-export default new Router({
+const originalPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch(err => err)
+}
+
+const router = new VueRouter ({
+  mode:'history',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'home',
+      component: home,
+      children:[
+        {path:'/main',component:main,name:'系统介绍'},
+        {path:'/user',component:user,name:'用户管理'},
+        {path:'/menu',component:menu,name:'菜单管理'},
+        { path: '/role', component: role, name: '角色管理' },
+        { path: '/dept', component: dept, name: '机构管理' },
+        { path: '/log', component: log, name: '日志管理' }
+      ]
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: login
+    }
+    ,{
+      path: '/404',
+      name: 'notFound',
+      component: NotFound
     }
   ]
 })
+
+router.beforeEach((to,from,next)=>{
+  let user = sessionStorage.getItem('user');
+  if (to.path=='/login'){
+    if (user){
+      next({path:'/'})
+    }else {
+      next()
+    }
+  }else {
+    if (!user){
+      next({path:'/login'});
+    }else {
+      next();
+    }
+  }
+})
+
+export default router
+
